4ec4e0afd1faa451a7a1a3c2dac51dd283b88996
sushxnth17 <sushanth0951s@gmail.com>
2026-05-20 21:05:04 +0530

    Add User model and automatic table creation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
-from .database import engine
+from .database import Base, engine
+from . import models
 
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def create_tables() -> None:
+	Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
