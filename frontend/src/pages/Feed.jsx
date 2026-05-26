3ec3660686120a3c5d2ed53a7d4b191e1db17703
sushxnth17 <sushanth0951s@gmail.com>
2026-05-26 21:10:51 +0530

    Connect frontend feed with backend API

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Feed() {
+    const [ideas, setIdeas] = useState([]);
+
+    useEffect(() => {
+        fetchIdeas();
+    }, []);
+
+    async function fetchIdeas() {
+        try {
+            const response = await api.get("/feed");
+            setIdeas(response.data);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <h1>Feed Page</h1>
+        <div>
+            <h1>Public Feed 🚀</h1>
+
+            {ideas.map((idea) => (
+                <div
+                    key={idea.id}
+                    style={{
+                        border:"1px solid gray",
+                        margin:"10px",
+                        padding:"10px"
+                    }}
+                >
+                    <h2>{idea.title}</h2>
+
+                    <p>
+                        {idea.description}
+                    </p>
+
+                    <p>
+                        ❤️ {idea.likes_count}
+                    </p>
+
+                </div>
+            ))}
+        </div>
     );
 }
 
