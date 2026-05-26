2722e5c943044c203449391878b1157173d4b28c
sushxnth17 <sushanth0951s@gmail.com>
2026-05-26 21:17:05 +0530

    Add trending page and frontend API integration

--- frontend/src/pages/Trending.jsx
+++ frontend/src/pages/Trending.jsx
@@ -1,6 +1,50 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Trending() {
+    const [ideas, setIdeas] = useState([]);
+
+    useEffect(() => {
+        fetchTrending();
+    }, []);
+
+    async function fetchTrending() {
+        try {
+            const response = await api.get("/trending");
+            setIdeas(response.data);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <h1>Trending Page</h1>
+        <div>
+            <h1>🔥 Trending Ideas</h1>
+
+            {ideas.map((idea)=>(
+                <div
+                    key={idea.id}
+                    style={{
+                        border:"1px solid gray",
+                        margin:"10px",
+                        padding:"10px"
+                    }}
+                >
+                    <h2>{idea.title}</h2>
+
+                    <p>{idea.description}</p>
+
+                    <p>
+                        ❤️ {idea.likes_count}
+                    </p>
+
+                    <p>
+                        💬 {idea.comments.length}
+                    </p>
+
+                </div>
+            ))}
+        </div>
     );
 }
 
