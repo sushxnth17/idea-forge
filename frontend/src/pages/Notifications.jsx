427dee1ece8f1ce05bfec3bca068b1947865d70b
sushxnth17 <sushanth0951s@gmail.com>
2026-05-26 21:19:20 +0530

    Add notifications page and frontend integration

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Notifications() {
+    const [notifications, setNotifications] = useState([]);
+
+    useEffect(() => {
+        fetchNotifications();
+    }, []);
+
+    async function fetchNotifications() {
+        try {
+            const response = await api.get(
+                "/users/notifications"
+            );
+
+            setNotifications(response.data);
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <h1>Notifications Page</h1>
+        <div>
+            <h1>🔔 Notifications</h1>
+
+            {notifications.map((notification)=>(
+                <div
+                    key={notification.id}
+                    style={{
+                        border:"1px solid gray",
+                        margin:"10px",
+                        padding:"10px"
+                    }}
+                >
+                    <p>
+                        {notification.message}
+                    </p>
+
+                    <small>
+                        Read:
+                        {" "}
+                        {notification.is_read
+                            ? "✅"
+                            : "❌"}
+                    </small>
+
+                </div>
+            ))}
+        </div>
     );
 }
 
