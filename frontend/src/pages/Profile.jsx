29a77443752eab0b8a9feba2947add21bf5820e4
sushxnth17 <sushanth0951s@gmail.com>
2026-05-26 21:15:46 +0530

    Add profile page and frontend integration

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Profile() {
+    const [profile, setProfile] = useState(null);
+
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+
+    async function fetchProfile() {
+        try {
+            const response = await api.get("/users/profile");
+            setProfile(response.data);
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    if (!profile) {
+        return <h2>Loading...</h2>;
+    }
+
     return (
-        <h1>Profile Page</h1>
+        <div>
+            <h1>My Profile 👤</h1>
+
+            <p>
+                <strong>Username:</strong>{" "}
+                {profile.username}
+            </p>
+
+            <p>
+                <strong>Email:</strong>{" "}
+                {profile.email}
+            </p>
+
+            <p>
+                <strong>Bio:</strong>{" "}
+                {profile.bio || "No bio"}
+            </p>
+        </div>
     );
 }
 
