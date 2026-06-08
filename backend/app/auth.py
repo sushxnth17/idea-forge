9b5bdb37f5ebcd0cadc9b6fc0a811d492c5d5077
sushxnth17 <sushanth0951s@gmail.com>
2026-06-08 18:15:54 +0530

    Fix deployment dependencies

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+import os
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt,JWTError
 from fastapi import Depends, HTTPException, status
@@ -7,9 +8,11 @@ from sqlalchemy.orm import Session
 from .database import get_db
 from .models import User
 
-SECRET_KEY = "secret_key_here"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = os.getenv("SECRET_KEY", "secret_key_here")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
+)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 def create_access_token(data: dict):
